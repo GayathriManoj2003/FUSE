b4817fd7283107c2ead8e599eddd1a215c830264
Gayathri Manoj <Gayathri Manoj>
2023-04-04 15:14:49 +0530

    Win and Lose window - cosmetic changes

--- client_game.py
+++ client_game.py
@@ -22,28 +22,71 @@ def waitWindow(win):
     win.blit(text, (pos_x1, pos_y1))
     pygame.display.update()
 
-def winning(win):
-    win.fill((128, 128, 128))
-    
-    font = pygame.font.SysFont("comicsans", 60)
-    text = font.render("You Win", True, (255,0,255))
+def winning(win, score, colour):
+    win.fill((0, 0, 0))
+    font = pygame.font.SysFont("impact", 60)
+    score_str = f"{score[0]} : {score[1]} "
+    score = font.render(score_str, True, colour)
+    inst = font.render("Press Enter to continue..", True, (0, 255, 0))
+    text = font.render("You Win :)", True, colour)
     pos_x1 = (screen_width - text.get_width())// 2
     pos_y1 = (screen_height- text.get_height())// 2
+
+    pos_x2 = (screen_width - score.get_width())// 2
+    pos_y2 = (screen_height - 3*score.get_height())// 2
+
+    pos_x3 = (screen_width - inst.get_width())// 2
+    pos_y3 = (screen_height + 3*inst.get_height())// 2
+
     win.blit(text, (pos_x1, pos_y1))
+    win.blit(score, (pos_x2, pos_y2))
+    win.blit(inst, (pos_x3, pos_y3))
+
     pygame.display.update()
-    pygame.time.delay(3000)
+    run = True
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                run = False
+
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_RETURN:
+                run = False
+
     menu_screen()
 
-def losing(win):
-    win.fill((128, 128, 128))
-    print("hi")
-    font = pygame.font.SysFont("comicsans", 60)
-    text = font.render("You Lose", True, (255,0,255))
+def losing(win, score, colour):
+    win.fill((0, 0, 0))
+    font = pygame.font.SysFont("impact", 60)
+    score_str = f"{score[0]} : {score[1]} "
+    score = font.render(score_str, True, colour)
+    inst = font.render("Press Enter to continue..", True, (0, 255, 0))
+    text = font.render("You Lose :(", True, colour)
+
     pos_x1 = (screen_width - text.get_width())// 2
     pos_y1 = (screen_height- text.get_height())// 2
+
+    pos_x2 = (screen_width - score.get_width())// 2
+    pos_y2 = (screen_height - 3*score.get_height())// 2
+
+    pos_x3 = (screen_width - inst.get_width())// 2
+    pos_y3 = (screen_height + 3*inst.get_height())// 2
+
     win.blit(text, (pos_x1, pos_y1))
+    win.blit(score, (pos_x2, pos_y2))
+    win.blit(inst, (pos_x3, pos_y3))
+
     pygame.display.update()
-    pygame.time.delay(3000)
+    run = True
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                run = False
+
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_RETURN:
+                run = False
+
     menu_screen()
 
 
@@ -107,29 +150,10 @@ def end_game(game, p):
     print("END GAME")
     print(game.wins)
     if game.wins[p] > game.wins[not p]:
-        winning(win)
-        
+        winning(win, game.wins, game.players[p].colour)
 
     else:
-        losing(win)
-        
-    # pygame.time.delay(800)
-    # win.fill((0,0,0))
-    # font = pygame.font.SysFont("impact", 50)
-    # colour = p.colour
-    # txtsurf = font.render("GAME COMPLETE.", True, (255, 255, 255))
-    # pos_x = (screen_width- txtsurf.get_width())// 2
-    # pos_y = (screen_height- txtsurf.get_height())// 2
-    # win.blit(txtsurf,(pos_x, pos_y))
-
-    # txtsurf1 = font.render(obj, True, colour)
-    # pos_x1 = (screen_width - txtsurf1.get_width())// 2
-    # pos_y1 = (screen_height- 3*txtsurf1.get_height())// 2
-    # win.blit(txtsurf1,(pos_x1, pos_y1))
-
-    # pygame.display.update()
-    # pygame.time.delay(2000)
-   
+        losing(win, game.wins, game.players[p].colour)
 
 def menu_screen():
     run = True
@@ -140,7 +164,7 @@ def menu_screen():
         win.fill((128, 128, 128))
         font = pygame.font.SysFont("comicsans", 60)
         # font = pygame.font.SysFont("impact", 50)
-        text = font.render("Click to Play!", True, (255,0,255))
+        text = font.render("Click to Connect!", True, (255,0,255))
         pos_x1 = (screen_width - text.get_width())// 2
         pos_y1 = (screen_height- text.get_height())// 2
         win.blit(text, (pos_x1, pos_y1))
